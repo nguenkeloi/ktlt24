6d90ae3803d0ed04075632c946b21a785eaf594f
nguyenkeloi <nguyenkeloi05102005@gmail.com>
2024-11-28 10:06:42 +0700

    Delete BTTH2 directory

--- BTTH2/bai2.3.py
+++
@@ -1,9 +0,0 @@
-print(" Nguyễn Kế Lợi")
-print("MSSV: 235752021610061")
-n = int(input("Enter a number: "))  # Nhập một số từ bàn phím
-
-# Kiểm tra số chẵn hay lẻ
-if n % 2 == 0:
-    print("EVEN Number")  # Nếu chia hết cho 2 thì là số chẵn
-else:
-    print("ODD Number")  # Nếu không chia hết cho 2 thì là số lẻ
--- BTTH2/bai2.4.py
+++
@@ -1,8 +0,0 @@
-print(" Nguyễn Kế Lợi")
-print("MSSV: 235752021610061")
-i=1;
-for j in range(2,10):
-    print("i:",i,"j:",j)
-    print(i,"/",j)
-    print(i/j);
-    
